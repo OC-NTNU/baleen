7b9425d8d0ca82425cbbecff468b2622d7ff350b
Erwin Marsi <emarsi@idi.ntnu.no>
2015-04-13 16:38:17 +0200

    print derivations to file

--- lib/baleen/trans/trace.py
+++ lib/baleen/trans/trace.py
@@ -4,7 +4,7 @@ Trace transformation of matches
 
 
 
-def print_derivations(matches):
+def print_derivations(matches, outf=None):
     """
     Print derivations showing transformation of original matches into
     transformed matches    
@@ -14,10 +14,14 @@ def print_derivations(matches):
     matches: pandas.DataFrame
         transformed matches    
     """
+    if isinstance(outf, str):
+        outf = open(outf, "w")
+        
     def print_steps(index, indent=0):
-        print(indent * " ", matches.at[index, "substr"]) 
+        print(indent * " ", matches.at[index, "substr"], file=outf) 
         for i in matches.at[index, "descendants"] or []:
-            print((indent + 4) * " ", "===", matches.at[i, "trans_name"], "===>")
+            print((indent + 4) * " ", "===", matches.at[i, "trans_name"], 
+                  "===>", file=outf)
             print_steps(i, indent + 8)
     
     # Select matches on top of derivations (exclude matches embedded in larger
@@ -26,8 +30,9 @@ def print_derivations(matches):
                             matches["ancestor"].isnull()]
     
     for n, index in enumerate(indices):
-        print(78 * "-")
-        print(n+1, ":", matches.at[index, "label"], "(", index, ")")
-        print(78 * "-")
+        print(78 * "-", file=outf)
+        print(n+1, ":", matches.at[index, "label"], "(", index, ")", 
+              file=outf)
+        print(78 * "-", file=outf)
         print_steps(index)
-        print()
+        print(file=outf)
